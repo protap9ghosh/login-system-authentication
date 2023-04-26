6c52d156ec4cbaca4517c4f181e6db26198dee18
Protap Ghosh <protapg518@gmail.com>
2023-04-26 22:31:12 +0600

    observe auth state change

--- src/Provider/AuthProviders.jsx
+++ src/Provider/AuthProviders.jsx
@@ -1,5 +1,5 @@
-import React, { createContext, useState } from 'react';
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
 import app from "../firebase/firebase.config"
 
 export const AuthContext = createContext(null);
@@ -15,6 +15,18 @@ const AuthProviders = ({ children }) => {
     const singIn = (email, password) => {
         return signInWithEmailAndPassword(auth, email, password);
     }
+
+    // observe auth state change
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            console.log('auth state change', currentUser);
+        });
+
+        return () => {
+            unsubscribe();
+        }
+    }, [])
+
 
     const authInfo = {
         user,
