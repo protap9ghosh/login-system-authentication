37e5f943aa723ee64ec39d4b1f660013b3e55c46
Protap Ghosh <protapg518@gmail.com>
2023-04-26 18:11:59 +0600

    add context API

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,8 @@ import React, { useContext } from 'react';
 import { AuthContext } from '../Provider/AuthProviders';
 
 const Home = () => {
-    const user = useContext(AuthContext);
+    const {user} = useContext(AuthContext);
+    console.log(user);
 
     return (
         <div>
